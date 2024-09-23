a2ea7acd4eb84cbd5baf760c3715c586d7c88931
Sugritsakorn Pattanaputhong <sugritsakorn.pat@thinkerfint.com>
2024-09-23 23:22:14 +0700

    Initial commit

---
+++ Source/Armpulse/Armpulse.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Armpulse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Armpulse, "Armpulse" );
+
+DEFINE_LOG_CATEGORY(LogArmpulse)
+ 
