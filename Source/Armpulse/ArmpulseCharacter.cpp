1a59bac04643051bf2cbbfd71847b3ca765648e3
Sugritsakorn Pattanaputhong <sugritsakorn.pat@thinkerfint.com>
2024-09-24 03:14:33 +0700

    wipe default code out

--- Source/Armpulse/ArmpulseCharacter.cpp
+++ Source/Armpulse/ArmpulseCharacter.cpp
@@ -22,8 +22,8 @@ AArmpulseCharacter::AArmpulseCharacter()
 	bUseControllerRotationRoll = false;
 
 	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Rotate character to moving direction
-	GetCharacterMovement()->RotationRate = FRotator(0.f, 640.f, 0.f);
+	//GetCharacterMovement()->bOrientRotationToMovement = true; // Rotate character to moving direction
+	//GetCharacterMovement()->RotationRate = FRotator(0.f, 640.f, 0.f);
 	GetCharacterMovement()->bConstrainToPlane = true;
 	GetCharacterMovement()->bSnapToPlaneAtStart = true;
 
--- Source/Armpulse/ArmpulseGameMode.cpp
+++ Source/Armpulse/ArmpulseGameMode.cpp
@@ -8,19 +8,19 @@
 AArmpulseGameMode::AArmpulseGameMode()
 {
 	// use our custom PlayerController class
-	PlayerControllerClass = AArmpulsePlayerController::StaticClass();
+	//PlayerControllerClass = AArmpulsePlayerController::StaticClass();
 
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	// // set default pawn class to our Blueprinted character
+	// static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
+	// if (PlayerPawnBPClass.Class != nullptr)
+	// {
+	// 	DefaultPawnClass = PlayerPawnBPClass.Class;
+	// }
 
-	// set default controller to our Blueprinted controller
-	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
-	if(PlayerControllerBPClass.Class != NULL)
-	{
-		PlayerControllerClass = PlayerControllerBPClass.Class;
-	}
+	// // set default controller to our Blueprinted controller
+	// static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
+	// if(PlayerControllerBPClass.Class != NULL)
+	// {
+	// 	PlayerControllerClass = PlayerControllerBPClass.Class;
+	// }
 }
