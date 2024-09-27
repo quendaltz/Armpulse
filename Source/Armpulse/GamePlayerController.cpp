178994d24aa1917ab3de82ea4ff233a9838b62c7
Sugritsakorn Pattanaputhong <sugritsakorn.pat@thinkerfint.com>
2024-09-28 00:36:06 +0700

    add custom controller for character class

---
+++ Source/Armpulse/GamePlayerController.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GamePlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+#include "GameCharacter.h"
+
+AGamePlayerController::AGamePlayerController()
+{
+	bShowMouseCursor = true;
+}
+
+void AGamePlayerController::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+}
+
+void AGamePlayerController::SetupInputComponent()
+{
+	// set up gameplay key bindings
+	Super::SetupInputComponent();
+
+	// Add Input Mapping Context
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+
+	// Set up action bindings
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent))
+	{
+		// Setup keyboard move input events
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AGamePlayerController::OnMoveTriggered);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Completed, this, &AGamePlayerController::OnMoveReleased);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Canceled, this, &AGamePlayerController::OnMoveReleased);
+		// EnhancedInputComponent->BindAction(SetDestinationClickAction, ETriggerEvent::Completed, this, &AArmpulsePlayerController::OnSetDestinationReleased);
+
+		// Setup touch input events
+		// EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Started, this, &AArmpulsePlayerController::OnInputStarted);
+		// EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Triggered, this, &AArmpulsePlayerController::OnTouchTriggered);
+		// EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Completed, this, &AArmpulsePlayerController::OnTouchReleased);
+		// EnhancedInputComponent->BindAction(SetDestinationTouchAction, ETriggerEvent::Canceled, this, &AArmpulsePlayerController::OnTouchReleased);
+	}
+	else
+	{
+		//UE_LOG(LogTemplateCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input Component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
+	}
+}
+
+void AGamePlayerController::OnMoveTriggered(const FInputActionValue& Value)
+{
+	AGameCharacter* PlayerCharacter = Cast<AGameCharacter>(GetPawn());
+	if (PlayerCharacter)
+	{
+		PlayerCharacter->MoveTriggered(Value);
+	}
+}
+
+void AGamePlayerController::OnMoveReleased(const FInputActionValue& Value)
+{
+    
+}
---
+++ Source/Armpulse/GamePlayerController.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GamePlayerController.generated.h"
+
+// Forward declaration
+class UNiagaraSystem;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS()
+class ARMPULSE_API AGamePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AGamePlayerController();
+
+	// Mapping Context
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	// Move Input Action
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* MoveAction;
+
+	// Combat Input Action
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* CombatAction;
+
+protected:
+	virtual void SetupInputComponent() override;
+
+	virtual void BeginPlay();
+
+	void OnMoveTriggered(const FInputActionValue& Value);
+	void OnMoveReleased(const FInputActionValue& Value);
+};
