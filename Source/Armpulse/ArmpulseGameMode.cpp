9f4c5e16b31d7c6f522f2273059b1a279c05e86b
Sugritsakorn Pattanaputhong <sugritsakorn.pat@thinkerfint.com>
2024-09-28 00:35:45 +0700

    test default template

--- Source/Armpulse/ArmpulseGameMode.cpp
+++ Source/Armpulse/ArmpulseGameMode.cpp
@@ -1,26 +1,27 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "ArmpulseGameMode.h"
-#include "ArmpulsePlayerController.h"
+#include "GamePlayerController.h"
 #include "ArmpulseCharacter.h"
+
 #include "UObject/ConstructorHelpers.h"
 
 AArmpulseGameMode::AArmpulseGameMode()
 {
 	// use our custom PlayerController class
-	//PlayerControllerClass = AArmpulsePlayerController::StaticClass();
+	PlayerControllerClass = AGamePlayerController::StaticClass();
 
-	// // set default pawn class to our Blueprinted character
-	// static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
-	// if (PlayerPawnBPClass.Class != nullptr)
-	// {
-	// 	DefaultPawnClass = PlayerPawnBPClass.Class;
-	// }
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/Character/BP_GameCharacter"));
+	if (PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
 
-	// // set default controller to our Blueprinted controller
-	// static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
-	// if(PlayerControllerBPClass.Class != NULL)
-	// {
-	// 	PlayerControllerClass = PlayerControllerBPClass.Class;
-	// }
+	// set default controller to our Blueprinted controller
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/Blueprints/Input/BP_GamePlayerController"));
+	if(PlayerControllerBPClass.Class != NULL)
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
 }
