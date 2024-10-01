cdf172947f76170b747db0016c4b14a20d9d4565
Sugritsakorn Pattanaputhong <sugritsakorn.pat@thinkerfint.com>
2024-10-01 15:48:56 +0700

    implement attack component class

--- Source/Armpulse/Character/Components/AttackComponent.cpp
+++ Source/Armpulse/Character/Components/AttackComponent.cpp
@@ -5,19 +5,30 @@
 #include "GameFramework/Actor.h"
 #include "Kismet/GameplayStatics.h"
 
-#include "Components/SphereComponent.h"
 #include "Components/BoxComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SphereComponent.h"
+
+#include "DrawDebugHelpers.h"
+
 
 // Sets default values for this component's properties
 UAttackComponent::UAttackComponent()
 {
+	PrimaryComponentTick.bCanEverTick = true;
+
     Damage = 10.0f;
     AttackRange = 100.0f;
     IsAoE = false;  // Default is single target attack
     AoERadius = 300.0f;
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+
+    SphereHitbox = CreateDefaultSubobject<USphereComponent>(TEXT("SphereHitbox"));
+    BoxHitbox = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxHitbox"));
+    CapsuleHitbox = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleHitbox"));
+
+    bIsHitboxActive = false;
 
 	HitboxComponent = CreateDefaultSubobject<USphereComponent>(TEXT("HitboxComponent"));
     AActor* OwnerActor = GetOwner();
@@ -36,11 +47,46 @@ UAttackComponent::UAttackComponent()
 void UAttackComponent::BeginPlay()
 {
 	Super::BeginPlay();
+
+    SphereHitbox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    BoxHitbox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    CapsuleHitbox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+    SphereHitbox->OnComponentBeginOverlap.AddDynamic(this, &UAttackComponent::OnHitboxOverlap);
+    BoxHitbox->OnComponentBeginOverlap.AddDynamic(this, &UAttackComponent::OnHitboxOverlap);
+    CapsuleHitbox->OnComponentBeginOverlap.AddDynamic(this, &UAttackComponent::OnHitboxOverlap);
 }
 
 void UAttackComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (SphereHitbox->IsActive())
+    {
+        SphereHitbox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
+        FVector SphereLocation = SphereHitbox->GetComponentLocation();
+        float SphereRadius = SphereHitbox->GetScaledSphereRadius();
+        DrawDebugSphere(GetWorld(), SphereLocation, SphereRadius, 12, FColor::Green, 0.0f, 2.0f);
+    }
+    else if (BoxHitbox->IsActive())
+    {
+        BoxHitbox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
+        FVector BoxLocation = BoxHitbox->GetComponentLocation();
+        FVector BoxExtent = BoxHitbox->GetScaledBoxExtent();
+        DrawDebugBox(GetWorld(), BoxLocation, BoxExtent, FColor::Blue);
+    }
+    else if (CapsuleHitbox->IsActive())
+    {
+        CapsuleHitbox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
+        FVector CapsuleLocation = CapsuleHitbox->GetComponentLocation();
+        float CapsuleRadius;
+        float CapsuleHalfHeight;
+        CapsuleHitbox->GetScaledCapsuleSize(CapsuleRadius, CapsuleHalfHeight);
+        DrawDebugCapsule(GetWorld(), CapsuleLocation, CapsuleHalfHeight, CapsuleRadius, FQuat::Identity, FColor::Red, false, 0.0f, 0, 2.0f);
+    }
 }
 
 void UAttackComponent::ExecuteAttack()
@@ -113,5 +159,92 @@ void UAttackComponent::GetActorsInRadius(TArray<AActor*>& OutActors)
         {
             OutActors.RemoveAt(i);
         }
+    }
+}
+
+// Function to create a dynamic hitbox (sphere, box, or capsule)
+void UAttackComponent::CreateHitbox(FVector Location, float Radius, float Height, FVector BoxExtent)
+{
+    // Set the location of the hitboxes
+    SphereHitbox->SetWorldLocation(Location);
+    BoxHitbox->SetWorldLocation(Location);
+    CapsuleHitbox->SetWorldLocation(Location);
+
+    // Set default to sphere hitbox if box extent is not set
+    if (BoxExtent == FVector(0.0f))
+    {
+        SetSphereHitbox(Radius);
+    }
+    else
+    {
+        SetBoxHitbox(BoxExtent);
+    }
+
+    // If height is set, assume capsule hitbox
+    if (Height > 0.0f)
+    {
+        SetCapsuleHitbox(Radius, Height);
+    }
+}
+
+// Enable the hitbox for collisions
+void UAttackComponent::EnableHitbox()
+{
+    bIsHitboxActive = true;
+
+    // Activate collision depending on the hitbox type
+    if (SphereHitbox->IsActive())
+    {
+        SphereHitbox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+    }
+    else if (BoxHitbox->IsActive())
+    {
+        BoxHitbox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+    }
+    else if (CapsuleHitbox->IsActive())
+    {
+        CapsuleHitbox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+    }
+}
+
+// Disable the hitbox for collisions
+void UAttackComponent::DisableHitbox()
+{
+    bIsHitboxActive = false;
+
+    SphereHitbox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    BoxHitbox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+    CapsuleHitbox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+// Set the sphere hitbox size
+void UAttackComponent::SetSphereHitbox(float Radius)
+{
+    SphereHitbox->SetSphereRadius(Radius);
+    SphereHitbox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+}
+
+// Set the box hitbox size
+void UAttackComponent::SetBoxHitbox(FVector BoxExtent)
+{
+    BoxHitbox->SetBoxExtent(BoxExtent);
+    BoxHitbox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+}
+
+// Set the capsule hitbox size
+void UAttackComponent::SetCapsuleHitbox(float Radius, float HalfHeight)
+{
+    CapsuleHitbox->SetCapsuleRadius(Radius);
+    CapsuleHitbox->SetCapsuleHalfHeight(HalfHeight);
+    CapsuleHitbox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+}
+
+// Handle overlap events
+void UAttackComponent::OnHitboxOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+    if (OtherActor && OtherActor != GetOwner())
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Hit: %s"), *OtherActor->GetName());
+        // Implement damage, effects, etc.
     }
 }
--- Source/Armpulse/Character/Components/AttackComponent.h
+++ Source/Armpulse/Character/Components/AttackComponent.h
@@ -37,8 +37,37 @@ public:
     // Function to handle hit detection
     void PerformHitDetection();
 
+    // Function to create a hitbox (sphere, box, or capsule)
+    void CreateHitbox(FVector Location, float Radius, float Height = 0.0f, FVector BoxExtent = FVector(0.0f));
+
+    // Function to enable or disable the hitbox
+    void EnableHitbox();
+    void DisableHitbox();
+
+    // Functions to dynamically adjust hitbox size
+    void SetSphereHitbox(float Radius);
+    void SetBoxHitbox(FVector BoxExtent);
+    void SetCapsuleHitbox(float Radius, float HalfHeight);
+
 protected:
 	virtual void BeginPlay() override;
+
+    UPROPERTY()
+    class USphereComponent* SphereHitbox;
+
+    UPROPERTY()
+    class UBoxComponent* BoxHitbox;
+
+    UPROPERTY()
+    class UCapsuleComponent* CapsuleHitbox;
+
+    // General flag to track the hitbox's active state
+    bool bIsHitboxActive;
+
+    // Collision function for when the hitbox overlaps with another actor
+    UFUNCTION()
+    void OnHitboxOverlap(UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+    bool bFromSweep, const FHitResult & SweepResult);
 
 public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
