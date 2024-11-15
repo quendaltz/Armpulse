c9b2038df26c62cea20c4ee93cf57bc3ab61085d
sugritsakorn pattanaputhong <apogixelleiz@gmail.com>
2024-11-16 04:22:32 +0700

    fix caster location update method

--- Source/Armpulse/Character/Skill/Skills/SwordRushSkill.cpp
+++ Source/Armpulse/Character/Skill/Skills/SwordRushSkill.cpp
@@ -9,67 +9,69 @@ void USwordRushSkill::ActivateSkill(AGameCharacter* Instigator)
 {
     if (Instigator)
     {
-        // float RushMultiplier = 0.8f; // 240% rush damage
-        // float RushHit = 3.0f; // 3 hits
-        // float RushDistance = 13.0f;
+        float RushMultiplier = 0.8f; // 240% rush damage
+        float RushHit = 3.0f; // 3 hits
+        float RushDistance = 13.0f;
 
-        // float CharacterAttackPower = 0.0f;
-        // auto DamageTypeClass = UDamageType::StaticClass();
-        // CharacterAttackPower = Instigator->GetStatusComponent()->GetAttackPower();
-        // if (CharacterAttackPower < 0.0f)
-        // {
-        //     CharacterAttackPower = 0.0f;
-        // }
+        float CharacterAttackPower = 0.0f;
+        auto DamageTypeClass = UDamageType::StaticClass();
+        CharacterAttackPower = Instigator->GetStatusComponent()->GetAttackPower();
+        if (CharacterAttackPower < 0.0f)
+        {
+            CharacterAttackPower = 0.0f;
+        }
 
-        // float SkillDamage = CharacterAttackPower * RushMultiplier;
+        float SkillDamage = CharacterAttackPower * RushMultiplier;
 
-        // // FVector ActorLocation = OwnerActor->GetActorLocation();
-        // // FVector RightVector = OwnerActor->GetActorRightVector();
-        // // UE_LOG(LogTemp, Log, TEXT("AttackRotation %s"), *AttackRotation.ToString());
-        // // UE_LOG(LogTemp, Log, TEXT("RightVector %s"), *RightVector.ToString());
+        // FVector ActorLocation = OwnerActor->GetActorLocation();
+        // FVector RightVector = OwnerActor->GetActorRightVector();
+        // UE_LOG(LogTemp, Log, TEXT("AttackRotation %s"), *AttackRotation.ToString());
+        // UE_LOG(LogTemp, Log, TEXT("RightVector %s"), *RightVector.ToString());
 
-        // FVector HitboxSpawnLocation = Instigator->GetForwardCharacterLocation(TargetHitboxRadius);
-        // FRotator AttackRotation = Instigator->GetActorRotation();
+        FVector HitboxSpawnLocation = Instigator->GetForwardCharacterLocation(TargetHitboxRadius);
+        FRotator AttackRotation = Instigator->GetActorRotation();
         
-        // FVector HitboxSize = FVector(TargetHitboxRadius, RushDistance, 0.0f);
-        // FCollisionShape Hitbox = FCollisionShape::MakeBox(HitboxSize);
-        // FQuat HitboxRotation = AttackRotation.Quaternion();
+        FVector HitboxSize = FVector(TargetHitboxRadius, RushDistance, 0.0f);
+        FCollisionShape Hitbox = FCollisionShape::MakeBox(HitboxSize);
+        FQuat HitboxRotation = AttackRotation.Quaternion();
         
-        // // Define collision parameters and check for overlaps
-        // FCollisionQueryParams CollisionParams;
-        // CollisionParams.AddIgnoredActor(Instigator); // Ignore self in overlap check
+        // Define collision parameters and check for overlaps
+        FCollisionQueryParams CollisionParams;
+        CollisionParams.AddIgnoredActor(Instigator); // Ignore self in overlap check
 
-        // if (AttackMontage)
-        // {
-        //     Instigator->ExecuteMontage(AttackMontage);
-        //     //Instigator->ExecuteAnimation(AttackAnimation);
-        // }
-        // GetOwner()->SetActorLocation(EndLocation, true, &HitResult);
+        if (AttackMontage)
+        {
+            Instigator->ExecuteMontage(AttackMontage);
+        }
 
-        // DrawDebugBox(GetWorld(), HitboxSpawnLocation, HitboxSize, HitboxRotation, FColor::Green, false, 1.0f); // Duration is 1 second
+        OwnerCharacter->GetDashComponent()->StartDash();
+        FVector CharacterTargetLocation = OwnerCharacter->GetForwardCharacterLocation(ActorCapsuleRadius + TargetHitboxRadius * 2);
+        OwnerCharacter->SetActorLocation(CharacterTargetLocation, true);
 
-        // // Check for enemies within the hitbox
-        // TArray<FOverlapResult> OverlapResults;
-        // bool bOverlap = GetWorld()->OverlapMultiByChannel(
-        //     OverlapResults,
-        //     HitboxSpawnLocation,
-        //     HitboxRotation,
-        //     ECC_GameTraceChannel1,
-        //     Hitbox,
-        //     CollisionParams
-        // );
+        DrawDebugBox(GetWorld(), HitboxSpawnLocation, HitboxSize, HitboxRotation, FColor::Green, false, 1.0f); // Duration is 1 second
 
-        // if (bOverlap)
-        // {
-        //     for (FOverlapResult& Overlap : OverlapResults)
-        //     {
-        //         if (AActor* OverlapActor = Overlap.GetActor())
-        //         {
-        //             // Apply damage or other effects
-        //             UE_LOG(LogTemp, Warning, TEXT("Overlap: %s"), *OverlapActor->GetName());
-        //             UGameplayStatics::ApplyDamage(OverlapActor, SkillDamage, OwnerInstigator, OwnerActor, DamageTypeClass);
-        //         }
-        //     }
-        // }
+        // Check for enemies within the hitbox
+        TArray<FOverlapResult> OverlapResults;
+        bool bOverlap = GetWorld()->OverlapMultiByChannel(
+            OverlapResults,
+            HitboxSpawnLocation,
+            HitboxRotation,
+            ECC_GameTraceChannel1,
+            Hitbox,
+            CollisionParams
+        );
+
+        if (bOverlap)
+        {
+            for (FOverlapResult& Overlap : OverlapResults)
+            {
+                if (AActor* OverlapActor = Overlap.GetActor())
+                {
+                    // Apply damage or other effects
+                    UE_LOG(LogTemp, Warning, TEXT("Overlap: %s"), *OverlapActor->GetName());
+                    UGameplayStatics::ApplyDamage(OverlapActor, SkillDamage, OwnerInstigator, OwnerActor, DamageTypeClass);
+                }
+            }
+        }
     }
 }
