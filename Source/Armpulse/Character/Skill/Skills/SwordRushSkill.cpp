ef0d713e3265d97091107ebaa114a85548be6dfd
sugritsakorn pattanaputhong <apogixelleiz@gmail.com>
2024-11-16 20:02:36 +0700

    adjust Sword Rush logic

--- Source/Armpulse/Character/Skill/Skills/SwordRushSkill.cpp
+++ Source/Armpulse/Character/Skill/Skills/SwordRushSkill.cpp
@@ -1,76 +1,89 @@
 #include "SwordRushSkill.h"
 
+#include "Engine/OverlapResult.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "Components/CapsuleComponent.h"
+
 #include "../../Character/GameCharacter.h"
 #include "../../Components/CharacterStatusComponent.h"
+#include "../../Components/Movement/CharacterDashComponent.h"
 
 #include "GameFramework/Actor.h"
 
-void USwordRushSkill::ActivateSkill(AGameCharacter* Instigator)
+USwordRushSkill::USwordRushSkill()
 {
-    if (Instigator)
+    // static ConstructorHelpers::FObjectFinder<UAnimMontage> MontageAsset(TEXT("/Game/Characters/Kwang/Montages/SwordRush_Montage"));
+    // if (MontageAsset.Succeeded())
+    // {
+    //     SwordRushMontage = MontageAsset;.Object;
+    // }
+}
+
+void USwordRushSkill::ActivateSkill(AGameCharacter* Instigator, AController* InstigatorController)
+{
+    if (!Instigator) return;
+
+    // skill properties
+    float RushMultiplier = 0.8f; // 240% rush total damage
+    float RushHit = 3.0f; // 3 hits
+    float RushDistance = 200.0f;
+    float RushSpeed = 1000.0f;
+
+    auto DamageTypeClass = UDamageType::StaticClass();
+    float CharacterAttackPower = 0.0f;
+    CharacterAttackPower = Instigator->GetStatusComponent()->GetAttackPower();
+    float TargetHitboxRadius = 0.0f;
+    TargetHitboxRadius = Instigator->GetCapsuleComponent()->GetScaledCapsuleRadius();
+    if (CharacterAttackPower < 0.0f)
     {
-        float RushMultiplier = 0.8f; // 240% rush damage
-        float RushHit = 3.0f; // 3 hits
-        float RushDistance = 13.0f;
+        CharacterAttackPower = 0.0f;
+    }
 
-        float CharacterAttackPower = 0.0f;
-        auto DamageTypeClass = UDamageType::StaticClass();
-        CharacterAttackPower = Instigator->GetStatusComponent()->GetAttackPower();
-        if (CharacterAttackPower < 0.0f)
+    float SkillDamage = CharacterAttackPower * RushMultiplier;
+
+    FRotator AttackRotation = Instigator->GetActorRotation();
+    FVector HitboxSpawnLocation = Instigator->GetForwardCharacterLocation(TargetHitboxRadius);
+    
+    FVector HitboxSize = FVector(TargetHitboxRadius * 2, RushDistance, 0.0f);
+    FCollisionShape Hitbox = FCollisionShape::MakeBox(HitboxSize);
+    FQuat HitboxRotation = AttackRotation.Quaternion();
+    
+    // Define collision parameters and check for overlaps
+    FCollisionQueryParams CollisionParams;
+    CollisionParams.AddIgnoredActor(Instigator); // Ignore self in overlap check
+
+    if (SwordRushMontage)
+    {
+        Instigator->ExecuteMontage(SwordRushMontage);
+    }
+
+    Instigator->GetDashComponent()->StartDash(RushDistance, RushSpeed);
+    FVector CharacterTargetLocation = Instigator->GetForwardCharacterLocation(TargetHitboxRadius * 2);
+    Instigator->SetActorLocation(CharacterTargetLocation, true);
+
+    DrawDebugBox(GetWorld(), HitboxSpawnLocation, HitboxSize, HitboxRotation, FColor::Green, false, 1.0f); // Duration is 1 second
+
+    // Check for enemies within the hitbox
+    TArray<FOverlapResult> OverlapResults;
+    bool bOverlap = GetWorld()->OverlapMultiByChannel(
+        OverlapResults,
+        HitboxSpawnLocation,
+        HitboxRotation,
+        ECC_GameTraceChannel1,
+        Hitbox,
+        CollisionParams
+    );
+
+    if (bOverlap)
+    {
+        for (FOverlapResult& Overlap : OverlapResults)
         {
-            CharacterAttackPower = 0.0f;
-        }
-
-        float SkillDamage = CharacterAttackPower * RushMultiplier;
-
-        // FVector ActorLocation = OwnerActor->GetActorLocation();
-        // FVector RightVector = OwnerActor->GetActorRightVector();
-        // UE_LOG(LogTemp, Log, TEXT("AttackRotation %s"), *AttackRotation.ToString());
-        // UE_LOG(LogTemp, Log, TEXT("RightVector %s"), *RightVector.ToString());
-
-        FVector HitboxSpawnLocation = Instigator->GetForwardCharacterLocation(TargetHitboxRadius);
-        FRotator AttackRotation = Instigator->GetActorRotation();
-        
-        FVector HitboxSize = FVector(TargetHitboxRadius, RushDistance, 0.0f);
-        FCollisionShape Hitbox = FCollisionShape::MakeBox(HitboxSize);
-        FQuat HitboxRotation = AttackRotation.Quaternion();
-        
-        // Define collision parameters and check for overlaps
-        FCollisionQueryParams CollisionParams;
-        CollisionParams.AddIgnoredActor(Instigator); // Ignore self in overlap check
-
-        if (AttackMontage)
-        {
-            Instigator->ExecuteMontage(AttackMontage);
-        }
-
-        OwnerCharacter->GetDashComponent()->StartDash();
-        FVector CharacterTargetLocation = OwnerCharacter->GetForwardCharacterLocation(ActorCapsuleRadius + TargetHitboxRadius * 2);
-        OwnerCharacter->SetActorLocation(CharacterTargetLocation, true);
-
-        DrawDebugBox(GetWorld(), HitboxSpawnLocation, HitboxSize, HitboxRotation, FColor::Green, false, 1.0f); // Duration is 1 second
-
-        // Check for enemies within the hitbox
-        TArray<FOverlapResult> OverlapResults;
-        bool bOverlap = GetWorld()->OverlapMultiByChannel(
-            OverlapResults,
-            HitboxSpawnLocation,
-            HitboxRotation,
-            ECC_GameTraceChannel1,
-            Hitbox,
-            CollisionParams
-        );
-
-        if (bOverlap)
-        {
-            for (FOverlapResult& Overlap : OverlapResults)
+            if (AActor* OverlapActor = Overlap.GetActor())
             {
-                if (AActor* OverlapActor = Overlap.GetActor())
-                {
-                    // Apply damage or other effects
-                    UE_LOG(LogTemp, Warning, TEXT("Overlap: %s"), *OverlapActor->GetName());
-                    UGameplayStatics::ApplyDamage(OverlapActor, SkillDamage, OwnerInstigator, OwnerActor, DamageTypeClass);
-                }
+                // Apply damage or other effects
+                UE_LOG(LogTemp, Warning, TEXT("Overlap: %s"), *OverlapActor->GetName());
+                UGameplayStatics::ApplyDamage(OverlapActor, SkillDamage, InstigatorController, Instigator, DamageTypeClass);
             }
         }
     }
--- Source/Armpulse/Character/Skill/Skills/SwordRushSkill.h
+++ Source/Armpulse/Character/Skill/Skills/SwordRushSkill.h
@@ -4,11 +4,16 @@
 #include "../CharacterSkillBase.h"
 #include "SwordRushSkill.generated.h"
 
-UCLASS()
+UCLASS(Blueprintable, BlueprintType)
 class ARMPULSE_API USwordRushSkill : public UCharacterSkillBase
 {
     GENERATED_BODY()
 
 public:
-    virtual void ActivateSkill(AGameCharacter* Instigator) override;
+    USwordRushSkill();
+    virtual void ActivateSkill(AGameCharacter* Instigator, AController* InstigatorController) override;
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+    UAnimMontage* SwordRushMontage;
 };
